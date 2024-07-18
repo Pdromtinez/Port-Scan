54a997e8ef00c4311af2daaf9e75f44e814f585e
Pdromtinez <pedromartinezt10@gmai.com>
2024-07-18 22:59:53 +0200

    (Python/portScanner) add-new-thread-option

--- App/portScanner.py
+++ App/portScanner.py
@@ -1,17 +1,35 @@
-import socket 
+import socket
+import threading
 
-ip = input("Ingrese la direccion IP a escanear")
 
-for port in range(1, 65535):
-    
+def scan_port(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(2)
-    
     result = sock.connect_ex((ip, port))
-    
     if result == 0:
-        print("Puerto abierto" + str(port))
-        sock.close()
-    else:
-        print("puerto cerrado " + str(port))
+        print(f"Puerto abierto: {port}")
+    sock.close()
+
+
+def scan_ports(ip, num_threads):
+    threads = []
+    for port in range(1, 65536):
+        thread = threading.Thread(target=scan_port, args=(ip, port))
+        threads.append(thread)
+        thread.start()
+
     
+        if len(threads) >= num_threads:
+            for thread in threads:
+                thread.join()
+            threads = []
+
+  
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    ip = input("Ingrese la dirección IP a escanear: ")
+    num_threads = int(input("Ingrese el número de hilos a usar: "))
+    
+    scan_ports(ip, num_threads)
